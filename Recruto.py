4ab712608c4d149b768380772ce8a0bb0cc0fa10
superxolet-source <superxolet@gmail.com>
2025-10-17 23:04:34 +0300

    Add files via upload

---
+++ Recruto.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def hello():
+    name = request.args.get('name', 'Recruto')
+    message = request.args.get('message', 'Давай дружить!')
+    return f"Hello {name}! {message}"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=80)
